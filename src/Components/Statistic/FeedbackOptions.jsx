866df37a75f1affcb0f674c01fec53ae27019e90
anastasyah86 <nastasyah86@gmail.com>
2021-04-02 22:37:37 +0300

    mess

--- src/Components/Statistic/FeedbackOptions.jsx
+++ src/Components/Statistic/FeedbackOptions.jsx
@@ -1,10 +1,9 @@
-
 import styles from './Statistic.module.css';
 
 const FeedbackOptions = ({ reviews, onAddRate }) => (
-  <ul className={styles.list}>
+  <ul className={styles.buttonList}>
     {reviews.map(button => (
-      <li key={button}>
+      <li key={button} className={styles.list}>
         <button className={styles.button__bad} onClick={onAddRate}>
           {button}
         </button>
