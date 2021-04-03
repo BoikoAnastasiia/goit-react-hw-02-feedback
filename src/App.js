db6d479fe440161ee104869ee6527fff6eab9300
anastasyah86 <nastasyah86@gmail.com>
2021-04-03 14:49:29 +0300

    done except the main thing :D

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './index.css';
 import Section from './Components/Statistic/';
 import FeedbackOptions from './Components/Statistic/FeedbackOptions';
 import Statistic from './Components/Statistic/Statistics';
+import Notification from './Components/Notification';
 
 class App extends Component {
   state = {
@@ -11,7 +12,7 @@ class App extends Component {
     bad: 3,
   };
 
-  onAddRate = options => {
+  onLeaveFeedback = options => {
     this.setState(prevstate => ({
       [options]: prevstate[options] + 1,
     }));
@@ -33,16 +34,23 @@ class App extends Component {
     const options = Object.keys(this.state);
     const total = this.calcTotalFeedback();
     const positivereviews = this.calcpositivePercentage();
+    const feedbacksValues = Object.values(this.state);
+
     return (
       <Section title="Please, give a feedback for us">
-        <FeedbackOptions reviews={options} onAddRate={this.onAddRate} />
-        <Statistic
-          good={this.state.good}
-          neutral={this.state.neutral}
-          bad={this.state.bad}
-          total={total}
-          positivePercentage={positivereviews}
-        />
+        <FeedbackOptions reviews={options} onAddRate={this.onLeaveFeedback} />
+
+        {feedbacksValues.some(rate => rate > 0) ? (
+          <Statistic
+            good={this.state.good}
+            neutral={this.state.neutral}
+            bad={this.state.bad}
+            total={total}
+            positivePercentage={positivereviews}
+          />
+        ) : (
+          <Notification message="No feedback given" />
+        )}
       </Section>
     );
   }
